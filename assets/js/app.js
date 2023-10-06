483e2e9c64f8fa6569bad6e6c56a3af20d5168d4
milad <milad13848@gmail.com>
2023-10-06 15:44:48 +0330

    #7 commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
 });
